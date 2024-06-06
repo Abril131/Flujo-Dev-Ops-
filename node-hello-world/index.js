ef8a879c115514a6567f7743339a4742487e8c28
Abril1231 <abriln2830@gmail.com>
2024-06-05 22:38:26 -0600

    nm

--- node-hello-world/index.js
+++ node-hello-world/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/api/v1/soa', (req, res) =>{
-    res.send("Hello, i love TAE");
+app.get('/api/v1/soaa', (req, res) =>{
+    res.send("Hello, Hola");
 });
 
 app.listen(port, () => {
